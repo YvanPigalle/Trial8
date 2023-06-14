fe33584730e28745a9019911e020eec52b4aada0
YvanPigalle <vanquishracer@gmail.com>
2023-06-14 21:49:09 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задача: Задайте двумерный массив. Напишите программу, 
 //которая упорядочит по убыванию элементы каждой строки двумерного массива.
-
+/*
 void FillArray(int [,] array)
 {
   for (int i=0; i<array.GetLength(0); i++)
@@ -49,3 +49,29 @@ PrintArray(ArrayRegression);
 Regression(ArrayRegression);
 Console.WriteLine("_______");
 PrintArray(ArrayRegression);
+*/
+//Задайте прямоугольный двумерный массив.
+//Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+void FillArray(int [,] array)
+{
+  for (int i=0; i<array.GetLength(0); i++)
+  {
+    for (int j=0; j<array.GetLength(1); j++)
+    {
+      array[i,j] = new Random().Next(1,10);
+    }
+  }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
